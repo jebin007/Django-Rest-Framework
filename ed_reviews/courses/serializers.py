3431ca925d122ee6abf4cbc04e398fe4ec008006
jebin007 <jevinsubedi@gmail.com>
2017-07-16 18:22:13 -0500

    Model Serializers:
    -Lets take a look at Django REST Framework's ModelSerializer class and how to use it to automatically turn models into resources.

---
+++ ed_reviews/courses/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from . import models
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        extra_kwargs = {
+            'email': {'write_only': True}
+        }
+        model = models.Review
+        fields = (
+            'id',
+            'course',
+            'name',
+            'email',
+            'review',
+            'created_at',
+        )
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Course
+        fields = (
+            'id',
+            'title',
+            'url',
+            )
