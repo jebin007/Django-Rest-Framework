76ed130d358b34ac8780c59a5cc744874680e087
jebin007 <jevinsubedi@gmail.com>
2017-07-16 21:01:05 -0500

    Generic CRUD:
    -Make your developer life a little easier with Django REST Framework's generic views.
    
    -modified ListCreateCourse view to generic view to make it simple and easy.
    
    -made generic views for Update and Delete all in one class using generic view.

--- ed_reviews/courses/urls.py
+++ ed_reviews/courses/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.ListCreateCourse.as_view(), name='course_list'),
+    url(r'(?P<pk>\d+)/$', views.RetrieveUpdateDestroyCourse.as_view(), name='course_detail')
 ]
--- ed_reviews/courses/views.py
+++ ed_reviews/courses/views.py
@@ -1,20 +1,13 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 
 from . import models
 from . import serializers
 # Create your views here.
 
-class ListCreateCourse(APIView):
-    def get(self, request, format=None):
-        courses = models.Course.objects.all()
-        serializer = serializers.CourseSerializer(courses, many=True)
-        return Response(serializer.data)
+class ListCreateCourse(generics.ListCreateAPIView):
+    queryset = models.Course.objects.all()
+    serializer_class = serializers.CourseSerializer
 
-    def post(self, request, format=None):
-        serializer = serializers.CourseSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
+class RetrieveUpdateDestroyCourse(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Course.objects.all()
+    serializer_class = serializers.CourseSerializer
