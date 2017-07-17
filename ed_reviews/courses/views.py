f19ead4d45dfa6bf74f1c78646b0968e9ad37cdf
jebin007 <jevinsubedi@gmail.com>
2017-07-16 19:01:47 -0500

    GET Requests with APIViews (1st API View)

--- ed_reviews/courses/views.py
+++ ed_reviews/courses/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
+from . import models
+from . import serializers
 # Create your views here.
+
+class ListCourse(APIView):
+    def get(self, request, format=None):
+        courses = models.Course.objects.all()
+        serializer = serializers.CourseSerializer(courses, many=True)
+        return Response(serializer.data)
